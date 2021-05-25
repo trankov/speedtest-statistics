a29ac84b2679c47f58bbfc1cf0f016eec2b97093
Aleksei Trankov <trankov@yandex.ru>
2021-05-25 23:02:59 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+# import sys
 import random
 import json
 import os
@@ -126,165 +127,116 @@ class Host(object):
     timezone: str = ip_info['timezone']
 
 
+def generate_sql(update_values: dict) -> str:
+    '''Generate table creation SQL for statistics'''
+
+    # Matching data types Python -> SQLite3
+    sql_types = {
+        str: "TEXT",
+        float: "REAL",
+        bool: "INTEGER",
+        int: "INTEGER"
+    }
+
+    # Making the request string, then return it
+    rq: str = ',\n\t'.join('{:<15} {} NOT NULL'.format(
+                        k, sql_types[type(v)]) for k, v in update_values.items())
+
+    return 'CREATE TABLE IF NOT EXISTS sessions\n\t' + \
+         '(\n\tid INTEGER PRIMARY KEY AUTOINCREMENT,\n\t' + rq + '\n\t);'
+
 
 def save_statistics(stats: dict, best: bool, dbname: str = 'speedtest.db') -> bool:
     '''Save `Speedtest()` and other statistics to sqlite3 `dbname` database.
 
-    `TABLE` fields are:
-     best: bool             whether test server best (True) or random (False)
-     download: float        download speed
-     upload: float          upload speed
-     ping: float            ping value
-     url: string            test url
-     test_lat: float        test server latitude
-     test_lon: float        test server longitude
-     test_name: str         test server name
-     test_country: str      test server country
-     test_cc: str           test server 2-letters country code
-     sponsor: str           test server sponsor
-     test_id: int           test server id
-     test_host: int         test host id
-     d: float               param d from Speedtest report (jitter?)
-     latency: float         latency value
-     timestamp: str         2021-05-23T15:52:54.399142Z
-     bytes_sent: int
-     bytes_received: int
-     client_ip: str         ↕︎ - the same as param name - ↕︎
-     client_lat: float
-     client_lon: float
-     client_isp: str        name of internet provider
-     client_country: str    2-letters country code
-     ip_remote: str         host ip address from outside
-     ip_local: str          host ip address in local network
-     name_remote: str       host name from outside
-     name_local: str        host name in local network
-     city: str              host city name for public ip address
-     region: str            the same but region
-     country: str           the same but country
-     timezone: str          the same but timezone
-     public_ip_lat: float   latitude for public ip address
-     public_ip_lon: float   longitude for public ip address
-     organization: str      the public ip owner
-     postal: str            postal code of organization above
-     vendor: str            hardware manufacturer
-     oui_hex: str           hardware hex oui
-     oui_base16: str        hardware base16 oui
-     vendor_address: str    hardware vendor address
-     mac_address: str       hardware mac address (hex)
-     unixtime: str          timestamp of transaction
+    For detailed fields description see `sqlite_strusture.md` document at:
+    https://github.com/trankov/speedtest-statistics/blob/main/sqlite_strusture.md
+
     '''
 
-    sqlite3.paramstyle = 'named'
-    con = sqlite3.connect(Path(CURDIR) / dbname)
-    cur = con.cursor()
-
-    cur.execute('''CREATE TABLE IF NOT EXISTS sessions
-                       (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        best INTEGER NOT NULL,
-                        download REAL NOT NULL,
-                        upload REAL NOT NULL,
-                        ping REAL NOT NULL,
-                        url TEXT NOT NULL,
-                        test_lat REAL NOT NULL,
-                        test_lon REAL NOT NULL,
-                        test_name TEXT NOT NULL,
-                        test_country TEXT NOT NULL,
-                        test_cc TEXT NOT NULL,
-                        sponsor TEXT NOT NULL,
-                        test_id INTEGER NOT NULL,
-                        test_host INTEGER NOT NULL,
-                        d REAL NOT NULL,
-                        latency REAL NOT NULL,
-                        timestamp TEXT NOT NULL,
-                        bytes_sent INTEGER NOT NULL,
-                        bytes_received INTEGER NOT NULL,
-                        client_ip TEXT NOT NULL,
-                        client_lat REAL NOT NULL,
-                        client_lon REAL NOT NULL,
-                        client_isp TEXT NOT NULL,
-                        client_country TEXT NOT NULL,
-                        ip_remote TEXT NOT NULL,
-                        ip_local TEXT NOT NULL,
-                        name_remote TEXT NOT NULL,
-                        name_local TEXT NOT NULL,
-                        city TEXT NOT NULL,
-                        region TEXT NOT NULL,
-                        country TEXT NOT NULL,
-                        timezone TEXT NOT NULL,
-                        public_ip_lat REAL NOT NULL,
-                        public_ip_lon REAL NOT NULL,
-                        organization TEXT NOT NULL,
-                        postal TEXT NOT NULL,
-                        vendor TEXT NOT NULL,
-                        oui_hex TEXT NOT NULL,
-                        oui_base16 TEXT NOT NULL,
-                        vendor_address TEXT NOT NULL,
-                        mac_address TEXT NOT NULL,
-                        unixtime REAL NOT NULL
-                        );''')
-
-    vendor = Vendor()
-    host = Host()
+    # Collecting info about related context
+    vendor: object = Vendor()
+    host: object = Host()
     # Get MAC Address in 1A:2B:3C:4D:5E:6F format
     mc_hex: str = hex(uuid.getnode())[2:].upper()
     macaddress: str = ":".join(mc_hex[i:i+2] for i in range(0, len(mc_hex), 2))
 
+    # Prepare the data structures
+
+    # Description of the data to be stored
+    # We also use explicit type conversion, because all data comes anyhow
     update_values = {
-        'best': best,
-        'download': stats['download'],
-        'upload': stats['upload'],
-        'ping': stats['ping'],
-        'url': stats['server']['url'],
-        'test_lat': stats['server']['lat'],
-        'test_lon': stats['server']['lon'],
-        'test_name': stats['server']['name'],
-        'test_country': stats['server']['country'],
-        'test_cc': stats['server']['cc'],
-        'sponsor': stats['server']['sponsor'],
-        'test_id': stats['server']['id'],
-        'test_host': stats['server']['host'],
-        'd': stats['server']['d'],
-        'latency': stats['server']['latency'],
-        'timestamp': stats['timestamp'],
-        'bytes_sent': stats['bytes_sent'],
-        'bytes_received': stats['bytes_received'],
-        'client_ip': stats['client']['ip'],
-        'client_lat': stats['client']['lat'],
-        'client_lon': stats['client']['lon'],
-        'client_isp': stats['client']['isp'],
-        'client_country': stats['client']['country'],
-        'ip_remote': host.ip_remote,
-        'ip_local': host.ip_local,
-        'name_remote': host.name_remote,
-        'name_local': host.name_local,
-        'city': host.city,
-        'region': host.region,
-        'country': host.country,
-        'timezone': host.timezone,
-        'public_ip_lat': host.location[0],
-        'public_ip_lon': host.location[1],
-        'organization': host.org,
-        'postal': host.postal,
-        'vendor': vendor.name(),
-        'oui_hex': vendor.oui_hex(),
-        'oui_base16': vendor.oui_base16(),
-        'vendor_address': vendor.address(),
-        'mac_address': macaddress,
-        'unixtime': datetime.now().timestamp()
+        'best': bool (best),
+        'download': float (stats['download']),
+        'upload': float (stats['upload']),
+        'ping': float (stats['ping']),
+        'url': str (stats['server']['url']),
+        'test_lat': float (stats['server']['lat']),
+        'test_lon': float (stats['server']['lon']),
+        'test_name': str (stats['server']['name']),
+        'test_country': str (stats['server']['country']),
+        'test_cc': str (stats['server']['cc']),
+        'sponsor': str (stats['server']['sponsor']),
+        'test_id': int (stats['server']['id']),
+        'test_host': str (stats['server']['host']),
+        'd': float (stats['server']['d']),
+        'latency': float (stats['server']['latency']),
+        'timestamp': str (stats['timestamp']),
+        'bytes_sent': int (stats['bytes_sent']),
+        'bytes_received': int (stats['bytes_received']),
+        'client_ip': str (stats['client']['ip']),
+        'client_lat': float (stats['client']['lat']),
+        'client_lon': float (stats['client']['lon']),
+        'client_isp': str (stats['client']['isp']),
+        'client_country': str (stats['client']['country']),
+        'ip_remote': str (host.ip_remote),
+        'ip_local': str (host.ip_local),
+        'name_remote': str (host.name_remote),
+        'name_local': str (host.name_local),
+        'city': str (host.city),
+        'region': str (host.region),
+        'country': str (host.country),
+        'timezone': str (host.timezone),
+        'public_ip_lat': float (host.location[0]),
+        'public_ip_lon': float (host.location[1]),
+        'organization': str (host.org),
+        'postal': str (host.postal),
+        'vendor': str (vendor.name()),
+        'oui_hex': str (vendor.oui_hex()),
+        'oui_base16': str (vendor.oui_base16()),
+        'vendor_address': str (vendor.address()),
+        'mac_address': str (macaddress),
+        'unixtime': float (datetime.now().timestamp())
     }
 
-    cur.execute('''INSERT INTO sessions (best, download, upload, ping, url, test_lat, test_lon, test_name, test_country, test_cc, sponsor, test_id, test_host, d, latency, timestamp, bytes_sent, bytes_received, client_ip, client_lat, client_lon, client_isp, client_country, ip_remote, ip_local, name_remote, name_local, city, region, country, timezone, public_ip_lat, public_ip_lon, organization, postal, vendor, oui_hex, oui_base16, vendor_address, mac_address, unixtime) VALUES (:best, :download, :upload, :ping, :url, :test_lat, :test_lon, :test_name, :test_country, :test_cc, :sponsor, :test_id, :test_host, :d, :latency, :timestamp, :bytes_sent, :bytes_received, :client_ip, :client_lat, :client_lon, :client_isp, :client_country, :ip_remote, :ip_local, :name_remote, :name_local, :city, :region, :country, :timezone, :public_ip_lat, :public_ip_lon, :organization, :postal, :vendor, :oui_hex, :oui_base16, :vendor_address, :mac_address, :unixtime)''', update_values)
+    sqlite3.paramstyle = 'named' # for better matching and security reasons
+    con = sqlite3.connect(Path(CURDIR) / dbname)
+    cur = con.cursor()
+
+    # Create database file and table if they don't exist
+    sql_table = generate_sql(update_values)
+    cur.execute(sql_table)
+
+    # Save statistics to database.sessions table
+    sql_request = '''INSERT INTO sessions ({}) VALUES ({})'''.format(
+        ', '.join(update_values),
+        ', '.join(f':{i}' for i in update_values)
+    )
+    cur.execute(sql_request, update_values)
+
+    # cur.execute('''INSERT INTO sessions (best, download, upload, ping, url, test_lat, test_lon, test_name, test_country, test_cc, sponsor, test_id, test_host, d, latency, timestamp, bytes_sent, bytes_received, client_ip, client_lat, client_lon, client_isp, client_country, ip_remote, ip_local, name_remote, name_local, city, region, country, timezone, public_ip_lat, public_ip_lon, organization, postal, vendor, oui_hex, oui_base16, vendor_address, mac_address, unixtime) VALUES (:best, :download, :upload, :ping, :url, :test_lat, :test_lon, :test_name, :test_country, :test_cc, :sponsor, :test_id, :test_host, :d, :latency, :timestamp, :bytes_sent, :bytes_received, :client_ip, :client_lat, :client_lon, :client_isp, :client_country, :ip_remote, :ip_local, :name_remote, :name_local, :city, :region, :country, :timezone, :public_ip_lat, :public_ip_lon, :organization, :postal, :vendor, :oui_hex, :oui_base16, :vendor_address, :mac_address, :unixtime)''', update_values)
 
     con.commit()
     con.close()
 
 class ProgressLine(object):
     '''Controls progress bar for multitreading requests'''
+
     def __init__(self):
         self.status = {
-                'start': '\033[48;5;227m \033[0m',
-                'end': '\033[0;0;42m \033[0m',
-                'empty': '\033[48;5;160m \033[0m'
+                'start': '\033[48;5;227m \033[0m', # yellow space
+                'end': '\033[0;0;42m \033[0m',     # green space
+                'empty': '\033[48;5;160m \033[0m'  # red space
             }
         self._line = []
 
@@ -293,13 +245,21 @@ class ProgressLine(object):
         return self._line
     @line.setter
     def line(self, value: int):
-        '''Initialize progress bar only once, emulating singleton'''
+        '''Just in case. Unnessessary but usefull.'''
         if self._line == []:
             self._line = [self.status['empty']] * value
-        # return self._line
+        return self._line
 
     def update(self, idx, count, **kwargs):
-        '''Fill cell `idx`:_int_ with `state`:_str_ via `self.status`:_dict_'''
+        '''Marks cells in progress bar.
+
+        Actually callback format is not documented and don't mentioned
+        in official documentation, but nevertheless it is available for use.
+        Callback getting tuple `(int, int)` and one positional argument,
+        which can be `start = True` or `end = True`. First int valued a current
+        request, second int contains total number of requests (always the same).
+
+        '''
 
         if self._line == []:
             self._line = [self.status['empty']] * count
@@ -321,14 +281,12 @@ class ProgressLine(object):
     def __repr__(self):
         return ''.join(self._line)
 
-def print_res(results):
+def print_res(results: dict):
+    '''Puts out formatted report about the speed test'''
+
     print('  Download: \033[4m{} Mbit/s\033[0m, Upload: \033[4m{} Mbit/s\033[0m, Ping: \033[4m{} ms\033[0m.\n  Provider: \033[38;5;198m{}, {}, {}\033[0m\n'\
         .format(
-            *[round(results[i] * 7.6294E-7, 2) for i in ['download', 'upload', 'ping']],
-            # round(results['download'] * 7.6294E-7, 2),
-            # round(results['upload'] * 7.6294E-7, 2),
-            # round(results['ping'] * 7.6294E-7, 2),
-
+            *[round(results[i] / 1048576, 2) for i in ['download', 'upload', 'ping']],
             results['server']['sponsor'],
             results['server']['name'],
             results['server']['country']
@@ -337,46 +295,65 @@ def print_res(results):
 
 
 if __name__ == '__main__':
-    import sys
 
-    speedtest = Speedtest()
-    srv = speedtest.get_servers()
-    rand_idx = random.choice(list(srv.keys()))
+    # Initialization of progress bar
     line = ProgressLine()
 
+    # Get the list of optional servers for further testing.
+    speedtest = Speedtest()
+    srv = speedtest.get_servers()
+
+    # Put out the main header
     print('\n\033[48;5;197m\033[38;5;220m',
           'Start Speedtest...'.center(65),
           '\033[0;0;m\n',
           sep=''
         )
 
-    # test at best server
+    # test at closest ("best") server
 
-    print('  \N{LONG RIGHTWARDS ARROW}  Test download speed from a best server...')
     speedtest.get_best_server()
+
+    # Testing download
+    print('  \N{LONG RIGHTWARDS ARROW}  Test download speed from the best server...')
     speedtest.download(callback=line.update)
     line.reset()
 
-    print('  \N{LONG LEFTWARDS ARROW}  Test upload speed at best server...')
+    # Testing upload
+    print('  \N{LONG LEFTWARDS ARROW}  Test upload speed at the best server...')
     speedtest.upload(callback=line.update)
     line.reset()
 
+    # Memorize the results for database, then show them
     res_best = speedtest.results.dict()
     print('\n\033[38;5;51mTest speed at best server finished.\033[0m')
     print_res(res_best)
 
-    del speedtest
-    speedtest = Speedtest()
+
 
     # test at random server
 
-    speedtest.get_servers([srv[rand_idx][0]['id']])
+    # object have to be reinitialized for changing the test server
+    # due to the peculiarities of the official library
+    del speedtest
+    speedtest = Speedtest()
 
-    print('  \N{LONG RIGHTWARDS ARROW}  Test download speed from a random server...')
+    # Now get the random server id
+    rand_idx = random.choice(list(srv.keys()))
+    if isinstance(srv[rand_idx], list):
+       srv_id = srv[rand_idx][0]['id']
+    else:
+        srv_id = srv[rand_idx]['id']
+
+    speedtest.get_servers([srv_id])
+
+    # Testing download
+    print('  \N{LONG RIGHTWARDS ARROW}  Test download speed from the random server...')
     speedtest.download(callback=line.update)
     line.reset()
 
-    print('  \N{LONG LEFTWARDS ARROW}  Test upload speed at random server...')
+    # Testing upload
+    print('  \N{LONG LEFTWARDS ARROW}  Test upload speed at the random server...')
     speedtest.upload(callback=line.update)
     line.reset()
 
@@ -385,9 +362,9 @@ if __name__ == '__main__':
     print_res(res_rand)
 
     print('Save statistics...')
-    save_statistics(stats=res_best, best=True)
-    save_statistics(stats=res_rand, best=False)
+    save_statistics(stats=res_best, best=True, dbname='test.db')
+    save_statistics(stats=res_rand, best=False, dbname='test.db')
     print('Statistics saved.')
     print('_'*65)
 
-    # 223710922.210356 * 7.6294E-7 == 170.678010991169
+# END
